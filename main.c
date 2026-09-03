0d0cb710005e3b89d8c3c0d80821428b6ec4815f
randytylor69 <Christophermaomao@gmail.com>
2026-09-02 21:02:29 -0400

    Setting up raylib, fastest raylib setup of my life

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "raylib.h"
 
+/* build command (yes i'm a lazy chud):
+ * clear && gcc -Wall -o spline main.c -lm -lraylib && ./spline
+ */
+
+#define WIN_WIDTH 900
+#define WIN_HEIGHT 600
+
+int main(){
+    InitWindow(WIN_WIDTH, WIN_HEIGHT, "Spline Rasterizer");
+    while (!WindowShouldClose()){
+	BeginDrawing();
+	ClearBackground(RED);
+	EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
